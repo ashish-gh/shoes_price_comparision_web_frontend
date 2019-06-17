cc7864876ab6c814f0e9523fd8b2027e8bb1b3f0
ashish ghimire <ashish.gh123@gmail.com>
2019-06-17 22:00:36 +0545

    Update dashboard

--- register.js
+++ register.js
@@ -1,35 +1,39 @@
 window.onload = function() {
 
-    const button = document.getElementById('login');
+    const button = document.getElementById('register');
   
     button.addEventListener('click', function() {
       const emialNode = document.getElementById('email');
       const passwordNode = document.getElementById('password');
       const email = emialNode.value;
       const password = passwordNode.value;
-      
+      const userType= "user";
+
       console.log(email,password);
       // create a http request similar to postman
       
-      fetch('http://localhost:8005/api/auth', {
+      fetch('http://localhost:8005/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           email: email,
-          password: password
+          password: password,
+          userType: userType
         })
       })
       .then(data => {
         return data.json();
+        console.log("first step");
       })
       .then(json => {
         if (json.status === 'success') {
           localStorage.setItem('Token', json.accessToken);
-          window.location.href = 'dashboard.html'
+          window.location.href = 'index.html'
         } else {
-          alert('email or password not matched');
+          alert('user already exists');
+          console.log("second step")
         }
       })
       .catch(error => {
