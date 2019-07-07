b4eca4fdfb93fd560a4e9abe3316288f30e7a11a
ashish ghimire <ashish.gh123@gmail.com>
2019-07-08 00:21:18 +0545

    Add services view for shoes description

---
+++ services/addReview.js
@@ -0,0 +1,66 @@
+const button = document.getElementById("addReview");
+button.addEventListener('click', addReview);
+
+function addReview(){
+    
+    const reviewNode = document.getElementById("review");
+    const shoeId = localStorage.getItem("itemId");
+    const userId = localStorage.getItem("userId");
+
+// to validate text field if they are empty or not
+
+    if(textValidatation(reviewNode)){
+        // check users is logged or not
+        if(localStorage.getItem("userId") == null){
+            alert("Please login first");
+            return;
+        }
+        addReviews(reviewNode.value, userId, shoeId)
+    }  
+}
+
+
+function textValidatation(review){
+    if(review.value.trim() == ''){
+      alert("Please enter your review");
+      review.focus();
+      return false;
+    }
+    return true;
+}
+
+
+
+function addReviews(review, userId, shoeId){
+
+    var today = new Date().toISOString().slice(0, 10)
+    fetch('http://localhost:3800/review', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          review: review,
+          userId: userId,
+          shoesId: shoeId,
+          reviewDate:today
+        })
+      })
+      .then(response=>{
+        return response.json();
+      })
+        .then(data => {        
+            if (data.status === 'success') {
+                alert('review added successfully');
+            } else {
+            alert('review not added');
+            }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      
+
+
+
+}
---
+++ services/getShoesDescription.js
@@ -0,0 +1,106 @@
+window.onload = function() {
+    getData();
+    getReview();
+
+}
+
+function getData(){
+    var txt='';
+    const shoesId = localStorage.getItem("itemId");
+
+    fetch('http://localhost:3800/shoes/'+shoesId, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'        
+        }
+      })
+      .then(response=>{
+        return response.json();
+      })
+      .then(data => {          
+          if(data.status == 'success'){
+              for(var i=0; i<data.dataResult.length ; i++){
+
+                txt +=' <h4 id="shoesId">' + data.dataResult[i].itemId +' </h4>'
+                txt +=' <h4>' + data.dataResult[i].shoesName +' </h4>'
+                txt +=' <h4>' + data.dataResult[i].shoesBrand+' </h4>'
+                txt +=' <h4>' + data.dataResult[i].shoesPrice+' </h4>'
+                txt +=' <h4>' + data.dataResult[i].shoesDescription +' </h4>'
+                txt +=' <h4>' + data.dataResult[i].shoesImageName +' </h4>'
+                txt +=' <h4>' + data.dataResult[i].shopId +' </h4>'
+                document.getElementById("shoesDetails").innerHTML = txt;
+
+                getShopDescription(data.dataResult[i].shopId);
+            }                          
+          }else if(data.status = 'fail'){
+              alert("No data found");
+          }
+      })
+      .catch(error =>{
+          console.log("Error: " +error);
+      })
+}
+
+function getShopDescription(shopId){
+    var txt='';
+
+    fetch('http://localhost:3800/shop/'+shopId, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'        
+        }
+      })
+      .then(response=>{
+        return response.json();
+      })
+      .then(data => {
+        if(data.status == 'success'){
+            for(var i=0; i<data.dataResult.length ; i++){
+                txt +=' <h4>' + data.dataResult[i].shopId +' </h4>'
+                txt +=' <h4>' + data.dataResult[i].shopName +' </h4>'
+                txt +=' <h4>' + data.dataResult[i].shopLocation+' </h4>'
+                txt +=' <h4>' + data.dataResult[i].shopDescription+' </h4>'
+
+                document.getElementById("shopDetails").innerHTML = txt;
+            }
+        }
+      })
+      .catch(error =>{
+        console.log("Error: " +error);
+    })
+}
+
+
+function getReview(){
+    var txt='';
+    fetch('http://localhost:3800/review', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'        
+        }
+      })
+      .then(response=>{
+        return response.json();
+      })
+      .then(data => {
+        if(data.status == 'success'){
+            
+            for(var i=0; i<data.dataResult.length ; i++){
+                txt +=' <h4>' + "shoes id " + data.dataResult[i].reviewId +' </h4>'
+                txt +=' <h4>' + data.dataResult[i].review +' </h4>'
+                txt +=' <h4>' + data.dataResult[i].reviewDate+' </h4>'
+                txt +='<br>'
+
+                document.getElementById("reviewDetails").innerHTML = txt;
+            }
+        }
+      })
+      .catch(error=>{
+        console.log("Error : "+ error);        
+      })
+}
+
+
